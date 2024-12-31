381c22bebc4099cff686d8836a9e0e14a283b780
Adriel Gonzalo Barrientos Mercado <122940027+Adrielgbm@users.noreply.github.com>
2024-12-31 14:11:00 -0300

    build: update gulpfile.js to include subdirectories in file paths

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ import ts from "gulp-typescript";
 
 gulp.task("scss", function () {
   return gulp
-    .src("./src/assets/styles/*.scss")
+    .src("./src/assets/styles/**/*.scss")
     .pipe(sassCompiler().on("error", sassCompiler.logError))
     .pipe(gulp.dest("./dist/styles"));
 });
 
 gulp.task("typescript", function () {
   return gulp
-    .src("./src/scripts/*.ts")
+    .src("./src/scripts/**/*.ts")
     .pipe(ts({ noImplicitAny: true }))
     .pipe(gulp.dest("./dist/scripts"));
 });
 
 gulp.task("watch", function () {
-  gulp.watch("./src/assets/styles/*.scss", gulp.series("scss"));
-  gulp.watch("./src/scripts/*.ts", gulp.series("typescript"));
+  gulp.watch("./src/assets/styles/**/*.scss", gulp.series("scss"));
+  gulp.watch("./src/scripts/**/*.ts", gulp.series("typescript"));
 });
 
 gulp.task("default", gulp.series("scss", "typescript", "watch"));
